307996b125de937363cc1c309e38d41690646e42
omer ben david <omerbendavid21@gmail.com>
2020-10-24 01:36:53 +0300

    added environment variable for host

--- mongo-inserter/settings/config.py
+++ mongo-inserter/settings/config.py
@@ -1,11 +1,11 @@
 import configparser
-
+import os
 
 config = configparser.ConfigParser()
 
 config.read('settings/config.ini')
 
 ALIAS = config.get('DATABASE', 'ALIAS')
-DB_HOST = config.get('DATABASE', 'HOST')
+DB_HOST = os.environ.get('MONGO')
 
 WEBSITE_COLLECTION = config.get('COLLECTIONS', 'WEBSITE')
