e4325fce5c5935b024633334a9b5407c49602735
omer ben david <omerbendavid21@gmail.com>
2020-10-25 13:44:57 +0200

    added setup for mongo-inserter

--- mongo-inserter/setup.py
+++ mongo-inserter/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mongo-inserter-omerbenda",
+    name="mongo_inserter_omerbenda",
     version="1.0.0",
     author="Omer Ben David",
     author_email="omerbendavid21@gmail.com",
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="http://192.168.5.124/omerbenda/omer-content-aggregator/mongo-inserter",
-    packages=setuptools.find_packages(),
+    packages=['api', 'classes', 'connection', 'settings'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
