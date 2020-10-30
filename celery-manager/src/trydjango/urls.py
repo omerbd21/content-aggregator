89dc046876ef92e1b5250760ce82d2ba3deac36d
omer ben david <omerbendavid21@gmail.com>
2020-10-30 13:51:33 +0200

    reverted to basic django server

--- celery-manager/src/trydjango/urls.py
+++ celery-manager/src/trydjango/urls.py
@@ -14,18 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-
-from pages.views import home_view, contact_view, about_view
+from django.urls import path
 
 
 urlpatterns = [
-    path('blog/', include('blog.urls')),
-    path('courses/', include('courses.urls')),
-    path('products/', include('products.urls')),
-    path('', home_view, name='home'),
-    path('about/<int:id>/', about_view, name='product-detail'),
-    path('contact/', contact_view),
     path('admin/', admin.site.urls),
 ]
 
