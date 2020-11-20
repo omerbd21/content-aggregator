a98b4de1d8c799d296544527d32addf2a2fec285
omer ben david <omerbendavid21@gmail.com>
2020-11-20 14:58:06 +0200

    finished the component, now making it more agile with env variables

--- celery-manager/src/tasks.py
+++ celery-manager/src/tasks.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+import json
 import time
 from datetime import datetime
 
@@ -20,7 +21,7 @@ def get_website_from_mongo(website_name):
         website_request = requests.get(url=SERVICES_URLS['mongo-fetcher'] + "/last_update/" + website_name)
         current_time = datetime.now()
         last_update = datetime.strptime(website_request.text[1:-1],
-                                                 "%m/%d/%Y, %H:%M:%S").timetuple()
+                                        "%m/%d/%Y, %H:%M:%S").timetuple()
         time_difference = time.mktime(datetime.timetuple(current_time)) - time.mktime(last_update)
         if time_difference / MINUTES_DIVIDER > 4:
             return False  # That means that the MongoDB isn't updated and we need to update it
@@ -40,5 +41,7 @@ def get_website_from_content_aggregator(website_name):
 
 @shared_task
 def update_website_in_mongo(website_info):
-    requests.put(url=SERVICES_URLS['mongo-inserter'] + "/update",
-                 data=website_info)
+    headers = {'content-type': 'application/json'}
+    code = requests.put(url=SERVICES_URLS['mongo-inserter'] + "/update",
+                        data=json.dumps(website_info), headers=headers)
+    print(code.headers['content-type'])
