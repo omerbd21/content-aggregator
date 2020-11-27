d98ddf75f9be9add9b5f83d728a92c76b3e32b7e
omer ben david <omerbendavid21@gmail.com>
2020-11-27 09:18:29 +0200

    added basic fetch

--- ui/ui/src/components/listItems.js
+++ ui/ui/src/components/listItems.js
@@ -4,6 +4,11 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import HttpIcon from '@material-ui/icons/Http';
 
+async function postData(url = '', data = {}) {
+    return await fetch('http://celery-manager-content-aggregator.192.168.5.60.nip.io/website_names')
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
 export const mainListItems = (
     <div>
         <ListItem button>
@@ -18,5 +23,7 @@ export const mainListItems = (
             </ListItemIcon>
             <ListItemText primary="Orders" />
         </ListItem>
+        <h1>{postData()}</h1>
     </div>
+
 );
