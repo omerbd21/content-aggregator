d051ca03324006bef735996c7e0f1283330e66c9
omer ben david <omerbendavid21@gmail.com>
2020-11-27 09:40:39 +0200

    added get request

--- ui/ui/src/components/listItems.js
+++ ui/ui/src/components/listItems.js
@@ -4,11 +4,28 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import HttpIcon from '@material-ui/icons/Http';
 
+// Example POST method implementation:
 async function postData(url = '', data = {}) {
-    return await fetch('http://celery-manager-content-aggregator.192.168.5.60.nip.io/website_names')
-        .then(response => response.json())
-        .then(data => console.log(data));
+    // Default options are marked with *
+    const response = await fetch(url, {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
 }
+
+postData('http://celery-manager-content-aggregator.192.168.5.60.nip.io/website_names' , { answer: 42 })
+    .then(data => {
+        console.log(data); // JSON data parsed by `data.json()` call
+    });
 export const mainListItems = (
     <div>
         <ListItem button>
@@ -23,7 +40,6 @@ export const mainListItems = (
             </ListItemIcon>
             <ListItemText primary="Orders" />
         </ListItem>
-        <h1>{postData()}</h1>
     </div>
 
 );
